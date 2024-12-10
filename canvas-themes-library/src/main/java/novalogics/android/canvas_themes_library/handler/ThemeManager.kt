248c5415ee55ca2ee5cb1dc14a2d0fe3b2fee41f
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-10 18:38:55 +0530

    feat [ handler ]: Add theme handler( ThemeManager )

---
+++ canvas-themes-library/src/main/java/novalogics/android/canvas_themes_library/handler/ThemeManager.kt
@@ -0,0 +1,74 @@
+package novalogics.android.canvas_themes_library.handler
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import novalogics.android.canvas_themes_library.themes.OrangeBlazeTheme
+
+object ThemeManager {
+
+    // For Light Mode : OrangeBlazeTheme
+     val OrangeBlazeLightColors = lightColorScheme(
+        primary = OrangeBlazeTheme.Light.primary,
+        onPrimary = OrangeBlazeTheme.Light.onPrimary,
+        primaryContainer = OrangeBlazeTheme.Light.primaryContainer,
+        onPrimaryContainer = OrangeBlazeTheme.Light.onPrimaryContainer,
+        secondary = OrangeBlazeTheme.Light.secondary,
+        onSecondary = OrangeBlazeTheme.Light.onSecondary,
+        secondaryContainer = OrangeBlazeTheme.Light.secondaryContainer,
+        onSecondaryContainer = OrangeBlazeTheme.Light.onSecondaryContainer,
+        tertiary = OrangeBlazeTheme.Light.tertiary,
+        onTertiary = OrangeBlazeTheme.Light.onTertiary,
+        tertiaryContainer = OrangeBlazeTheme.Light.tertiaryContainer,
+        onTertiaryContainer = OrangeBlazeTheme.Light.onTertiaryContainer,
+        error = OrangeBlazeTheme.Light.error,
+        errorContainer = OrangeBlazeTheme.Light.errorContainer,
+        onError = OrangeBlazeTheme.Light.onError,
+        onErrorContainer = OrangeBlazeTheme.Light.onErrorContainer,
+        background = OrangeBlazeTheme.Light.background,
+        onBackground = OrangeBlazeTheme.Light.onBackground,
+        surface = OrangeBlazeTheme.Light.surface,
+        onSurface = OrangeBlazeTheme.Light.onSurface,
+        surfaceVariant = OrangeBlazeTheme.Light.surfaceVariant,
+        onSurfaceVariant = OrangeBlazeTheme.Light.onSurfaceVariant,
+        outline = OrangeBlazeTheme.Light.outline,
+        inverseOnSurface = OrangeBlazeTheme.Light.inverseOnSurface,
+        inverseSurface = OrangeBlazeTheme.Light.inverseSurface,
+        inversePrimary = OrangeBlazeTheme.Light.inversePrimary,
+        surfaceTint = OrangeBlazeTheme.Light.surfaceTint,
+        outlineVariant = OrangeBlazeTheme.Light.outlineVariant,
+        scrim = OrangeBlazeTheme.Light.scrim
+    )
+
+    // For Dark Mode : OrangeBlazeTheme
+     val OrangeBlazeDarkColors = darkColorScheme(
+        primary = OrangeBlazeTheme.Dark.primary,
+        onPrimary = OrangeBlazeTheme.Dark.onPrimary,
+        primaryContainer = OrangeBlazeTheme.Dark.primaryContainer,
+        onPrimaryContainer = OrangeBlazeTheme.Dark.onPrimaryContainer,
+        secondary = OrangeBlazeTheme.Dark.secondary,
+        onSecondary = OrangeBlazeTheme.Dark.onSecondary,
+        secondaryContainer = OrangeBlazeTheme.Dark.secondaryContainer,
+        onSecondaryContainer = OrangeBlazeTheme.Dark.onSecondaryContainer,
+        tertiary = OrangeBlazeTheme.Dark.tertiary,
+        onTertiary = OrangeBlazeTheme.Dark.onTertiary,
+        tertiaryContainer = OrangeBlazeTheme.Dark.tertiaryContainer,
+        onTertiaryContainer = OrangeBlazeTheme.Dark.onTertiaryContainer,
+        error = OrangeBlazeTheme.Dark.error,
+        errorContainer = OrangeBlazeTheme.Dark.errorContainer,
+        onError = OrangeBlazeTheme.Dark.onError,
+        onErrorContainer = OrangeBlazeTheme.Dark.onErrorContainer,
+        background = OrangeBlazeTheme.Dark.background,
+        onBackground = OrangeBlazeTheme.Dark.onBackground,
+        surface = OrangeBlazeTheme.Dark.surface,
+        onSurface = OrangeBlazeTheme.Dark.onSurface,
+        surfaceVariant = OrangeBlazeTheme.Dark.surfaceVariant,
+        onSurfaceVariant = OrangeBlazeTheme.Dark.onSurfaceVariant,
+        outline = OrangeBlazeTheme.Dark.outline,
+        inverseOnSurface = OrangeBlazeTheme.Dark.inverseOnSurface,
+        inverseSurface = OrangeBlazeTheme.Dark.inverseSurface,
+        inversePrimary = OrangeBlazeTheme.Dark.inversePrimary,
+        surfaceTint = OrangeBlazeTheme.Dark.surfaceTint,
+        outlineVariant = OrangeBlazeTheme.Dark.outlineVariant,
+        scrim = OrangeBlazeTheme.Dark.scrim
+    )
+}
