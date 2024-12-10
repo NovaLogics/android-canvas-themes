4bcdb16015f6d16e2b6d6528040b310e1084229d
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-10 22:44:08 +0530

    feat [ mc-i ]: Add main theme access file

---
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/MaterialCanvas.kt
@@ -0,0 +1,15 @@
+package novalogics.android.canvas_themes
+
+import androidx.compose.material3.ColorScheme
+
+interface MaterialCanvas {
+
+    fun lightThemeOrangeBlaze(): ColorScheme
+    fun darkThemeOrangeBlaze(): ColorScheme
+
+    fun lightThemeOrangeLustre(): ColorScheme
+    fun darkThemeOrangeLustre(): ColorScheme
+
+    fun lightThemeBlackWhite(): ColorScheme
+    fun darkThemeBlackWhite(): ColorScheme
+}
