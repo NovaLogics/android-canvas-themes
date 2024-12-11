5c440f4ad1c99be6510b1d3424345486adf848a7
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 15:18:27 +0530

    feat [ main-mc ]: Update Material Canvas Style

--- android-canvas-themes/src/main/java/novalogics/android/canvas_themes/MaterialCanvas.kt
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/MaterialCanvas.kt
@@ -1,15 +1,18 @@
 package novalogics.android.canvas_themes
 
+
 import androidx.compose.material3.ColorScheme
+import novalogics.android.canvas_themes.handler.ThemeManager
 
-interface MaterialCanvas {
+class MaterialCanvas {
+    private val themeManager: ThemeManager = ThemeManager
 
-    fun lightThemeOrangeBlaze(): ColorScheme
-    fun darkThemeOrangeBlaze(): ColorScheme
+    fun getOrangeBlazeLightColors(): ColorScheme = themeManager.OrangeBlazeLightColors
+    fun getOrangeBlazeDarkColors(): ColorScheme = themeManager.OrangeBlazeDarkColors
 
-    fun lightThemeOrangeLustre(): ColorScheme
-    fun darkThemeOrangeLustre(): ColorScheme
+    fun getOrangeLustreLightColors(): ColorScheme = themeManager.OrangeLustreLightColors
+    fun getOrangeLustreDarkColors(): ColorScheme = themeManager.OrangeLustreDarkColors
 
-    fun lightThemeBlackWhite(): ColorScheme
-    fun darkThemeBlackWhite(): ColorScheme
+    fun getBlackWhiteLightColors(): ColorScheme = themeManager.BlackWhiteLightColors
+    fun getBlackWhiteDarkColors(): ColorScheme = themeManager.BlackWhiteDarkColors
 }
