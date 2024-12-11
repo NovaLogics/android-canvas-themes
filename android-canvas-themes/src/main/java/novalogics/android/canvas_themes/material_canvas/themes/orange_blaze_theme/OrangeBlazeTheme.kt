51145e64ac59bd9124ee824a1cad6f9c4f738d65
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 19:24:42 +0530

    lib | feat [ theme ]: Add theme (OrangeBlazeTheme)

---
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/material_canvas/themes/orange_blaze_theme/OrangeBlazeTheme.kt
@@ -0,0 +1,8 @@
+package novalogics.android.canvas_themes.material_canvas.themes.orange_blaze_theme
+
+object OrangeBlazeTheme {
+    val lightColors = OrangeBlazeColors.Light
+    val darkColors = OrangeBlazeColors.Dark
+    val lightColorScheme  = OrangeBlazeScheme.LightColorScheme
+    val darkColorScheme = OrangeBlazeScheme.DarkColorScheme
+}
