b1fd7cc4ff23727594221437937d058b2cf5fadf
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 20:54:53 +0530

    lib | feat [ theme-purple ]: Add theme (PurpleBlazeTheme)

---
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/material_canvas/themes/purple_blaze_theme/PurpleBlazeTheme.kt
@@ -0,0 +1,8 @@
+package novalogics.android.canvas_themes.material_canvas.themes.purple_blaze_theme
+
+object PurpleBlazeTheme {
+    val lightColors = PurpleBlazeColors.Light
+    val darkColors = PurpleBlazeColors.Dark
+    val lightColorScheme  = PurpleBlazeScheme.LightColorScheme
+    val darkColorScheme = PurpleBlazeScheme.DarkColorScheme
+}
