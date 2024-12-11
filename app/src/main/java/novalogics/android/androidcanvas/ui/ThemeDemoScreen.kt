a76be06a09c02ec17ba36c9aa8e5998263c7aff4
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 18:47:45 +0530

    demo | feat [ screen ]: Update demo screen

--- app/src/main/java/novalogics/android/androidcanvas/ui/ThemeDemoScreen.kt
+++ app/src/main/java/novalogics/android/androidcanvas/ui/ThemeDemoScreen.kt
@@ -1,11 +1,14 @@
 package novalogics.android.androidcanvas.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
@@ -16,13 +19,16 @@ import androidx.compose.material3.*
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import novalogics.android.androidcanvas.R
 import novalogics.android.canvas_themes.material_canvas.MaterialCanvas
 
 @Composable
@@ -50,86 +56,121 @@ fun ThemeDemoScreen() {
                 .fillMaxSize()
                 .padding(12.dp)
         ) {
-            // Title
-            Text(
-                "Material Canvas \nGrayscale Theme Demo",
-                color = onBackground,
-                style = MaterialTheme.typography.titleLarge,
-                textAlign = TextAlign.Center,
+            Card(
                 modifier = Modifier
-                    .fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Primary Button
-            Button(
-                onClick = {},
-                colors = buttonColors(containerColor = primary),
-                elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(topStart = 16.dp, bottomEnd = 16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                colors = CardDefaults.cardColors(containerColor = colorScheme.scrim),
             ) {
                 Text(
-                    "Primary Button",
-                    color = onPrimary,
+                    "Grayscale Theme : Dark",
+                    color = onBackground,
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                    fontSize = 24.sp,
+                    letterSpacing = 0.8.sp,
                     fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
                     fontFamily = FontFamily.Serif,
-                    letterSpacing = 0.8.sp,
-                    modifier = Modifier.padding(
-                        top = 4.dp,
-                        bottom = 4.dp,
-                    )
+                    modifier = Modifier
+                        .fillMaxWidth().padding(16.dp)
                 )
             }
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
-            // Secondary Button
-            Button(
-                onClick = { /*TODO*/ },
-                colors = buttonColors(containerColor = secondary),
-                elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                Text(
-                    "Secondary Button",
-                    color = onSecondary,
-                    fontWeight = FontWeight.W500,
-                    fontSize = 18.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    letterSpacing = 0.8.sp,
-                    modifier = Modifier.padding(
-                        top = 4.dp,
-                        bottom = 4.dp,
-                    )
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
 
-            // Tertiary Button
-            Button(
-                onClick = { },
-                colors = buttonColors(containerColor = colorScheme.tertiary),
-                elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp)
-            ) {
-                Text(
-                    "Tertiary Button", color = colorScheme.onTertiary,
-                    fontWeight = FontWeight.W500,
-                    fontSize = 18.sp,
-                    fontFamily = FontFamily.Monospace,
-                    letterSpacing = 0.5.sp,
-                    modifier = Modifier.padding(
-                        top = 4.dp,
-                        bottom = 4.dp,
-                    )
+                Column(
+                    modifier = Modifier,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    // Primary Button
+                    Button(
+                        onClick = {},
+                        colors = buttonColors(containerColor = primary),
+                        elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
+                    ) {
+                        Text(
+                            "Primary Button",
+                            color = onPrimary,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily.Serif,
+                            letterSpacing = 0.8.sp,
+                            modifier = Modifier.padding(
+                                top = 4.dp,
+                                bottom = 4.dp,
+                            )
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    // Secondary Button
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = buttonColors(containerColor = secondary),
+                        elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
+                    ) {
+                        Text(
+                            "Secondary Button",
+                            color = onSecondary,
+                            fontWeight = FontWeight.W500,
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily.SansSerif,
+                            letterSpacing = 0.8.sp,
+                            modifier = Modifier.padding(
+                                top = 4.dp,
+                                bottom = 4.dp,
+                            )
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    // Tertiary Button
+                    Button(
+                        onClick = { },
+                        colors = buttonColors(containerColor = colorScheme.tertiary),
+                        elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
+                    ) {
+                        Text(
+                            "Tertiary Button", color = colorScheme.onTertiary,
+                            fontWeight = FontWeight.W500,
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily.Monospace,
+                            letterSpacing = 0.5.sp,
+                            modifier = Modifier.padding(
+                                top = 4.dp,
+                                bottom = 4.dp,
+                            )
+                        )
+                    }
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.ic_night_mode),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(132.dp)  // Size of the image
+                        .clip(CircleShape)  // Clip the image into a circle
+                        .border(3.dp, colorScheme.surfaceVariant, CircleShape)  // Add a circular border
+                        .align(Alignment.CenterVertically)  // Align it vertically
                 )
+
             }
-         
             Spacer(modifier = Modifier.height(32.dp))
 
             // Sliders to show the surfaceVariant and outline
             Text(
                 "Surface Variant",
-                color = colorScheme.surfaceVariant,
-                fontWeight = FontWeight.W500,
-                fontSize = 16.sp,
+                color = colorScheme.onSurfaceVariant,
+                fontWeight = FontWeight.W200,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif,
+
                 letterSpacing = 0.6.sp,
             )
             Spacer(modifier = Modifier.height(2.dp))
@@ -159,14 +200,14 @@ fun ThemeDemoScreen() {
                 ) {
                     // First Card
                     CardItem(
-                        text = "Surface Card",
+                        text = "Surface\nCard",
                         icon = Icons.Filled.Favorite,
                         surface = surface,
                         onSurface = onSurface
                     )
                     // Second Card
                     CardItem(
-                        text = "Error Box ",
+                        text = "Error\nBox ",
                         icon = Icons.Filled.Warning,
                         surface = error,
                         onSurface = onError
@@ -181,14 +222,14 @@ fun ThemeDemoScreen() {
                 ) {
                     // Third Card
                     CardItem(
-                        text = "Inverse Surface",
+                        text = "Inverse\nSurface",
                         icon = Icons.Filled.CheckCircle,
                         surface = colorScheme.inverseSurface,
                         onSurface = colorScheme.inverseOnSurface
                     )
                     // Fourth Card
                     CardItem(
-                        text = "Scrim Overlay",
+                        text = "Scrim\nOverlay",
                         icon = Icons.Filled.PlayArrow,
                         surface = colorScheme.scrim.copy(alpha = 0.3f),
                         onSurface = colorScheme.onSurface
@@ -220,7 +261,7 @@ fun CardItem(text: String, icon: ImageVector, surface: Color, onSurface: Color) 
     Card(
         modifier = Modifier
             .width(180.dp)
-            .height(120.dp)
+            .height(160.dp)
             .padding(8.dp),
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
@@ -244,8 +285,9 @@ fun CardItem(text: String, icon: ImageVector, surface: Color, onSurface: Color) 
                 text = text,
                 color = onSurface,
                 textAlign = TextAlign.Center,
-                fontWeight = FontWeight.W500,
-                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif,
                 letterSpacing = 0.5.sp
             )
         }
