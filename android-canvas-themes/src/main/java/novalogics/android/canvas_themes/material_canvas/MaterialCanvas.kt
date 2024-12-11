53f3249c5eba8580718bc6520fdefd4ffe5ed6f6
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 21:05:13 +0530

    lib | feat [ MaterialCanvas ]: Update theme data

--- android-canvas-themes/src/main/java/novalogics/android/canvas_themes/material_canvas/MaterialCanvas.kt
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/material_canvas/MaterialCanvas.kt
@@ -1,11 +1,21 @@
 package novalogics.android.canvas_themes.material_canvas
 
+import novalogics.android.canvas_themes.material_canvas.themes.amber_blaze_theme.AmberBlazeTheme
+import novalogics.android.canvas_themes.material_canvas.themes.blue_blaze_theme.BlueBlazeTheme
 import novalogics.android.canvas_themes.material_canvas.themes.grayscale_theme.GrayscaleTheme
+import novalogics.android.canvas_themes.material_canvas.themes.green_blaze_theme.GreenBlazeTheme
 import novalogics.android.canvas_themes.material_canvas.themes.orange_blaze_theme.OrangeBlazeTheme
+import novalogics.android.canvas_themes.material_canvas.themes.purple_blaze_theme.PurpleBlazeTheme
+import novalogics.android.canvas_themes.material_canvas.themes.red_blaze_theme.RedBlazeTheme
+import novalogics.android.canvas_themes.material_canvas.themes.teal_blaze_theme.TealBlazeTheme
 
 object MaterialCanvas {
-
     val grayscaleTheme = GrayscaleTheme
     val orangeBlazeTheme = OrangeBlazeTheme
-
+    val amberBlazeTheme = AmberBlazeTheme
+    val blueBlazeTheme = BlueBlazeTheme
+    val greenBlazeTheme = GreenBlazeTheme
+    val purpleBlazeTheme = PurpleBlazeTheme
+    val redBlazeTheme = RedBlazeTheme
+    val tealBlazeTheme = TealBlazeTheme
 }
