f01872596a40669ecf099591114af0b5e2ea1a92
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 20:58:20 +0530

    lib | feat [ theme-red ]: Add theme (RedBlazeTheme)

---
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/material_canvas/themes/red_blaze_theme/RedBlazeTheme.kt
@@ -0,0 +1,8 @@
+package novalogics.android.canvas_themes.material_canvas.themes.red_blaze_theme
+
+object RedBlazeTheme {
+    val lightColors = RedBlazeColors.Light
+    val darkColors = RedBlazeColors.Dark
+    val lightColorScheme  = RedBlazeScheme.LightColorScheme
+    val darkColorScheme = RedBlazeScheme.DarkColorScheme
+}
