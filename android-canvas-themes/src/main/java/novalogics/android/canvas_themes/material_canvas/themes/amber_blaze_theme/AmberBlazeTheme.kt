21c1f5887a9dc5c13471c757dee74eb40a99c7ab
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-11 20:45:11 +0530

    lib | feat [ theme ]: Add theme (AmberBlazeTheme)

---
+++ android-canvas-themes/src/main/java/novalogics/android/canvas_themes/material_canvas/themes/amber_blaze_theme/AmberBlazeTheme.kt
@@ -0,0 +1,8 @@
+package novalogics.android.canvas_themes.material_canvas.themes.amber_blaze_theme
+
+object AmberBlazeTheme {
+    val lightColors = AmberBlazeColors.Light
+    val darkColors = AmberBlazeColors.Dark
+    val lightColorScheme  = AmberBlazeScheme.LightColorScheme
+    val darkColorScheme = AmberBlazeScheme.DarkColorScheme
+}
