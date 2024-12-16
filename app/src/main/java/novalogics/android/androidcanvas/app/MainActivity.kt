86863b0c36e7ff31f02108cf432ab4e57865b29f
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-17 00:03:05 +0530

    build [ lib ]: Update release info [v1.0.0]

--- app/src/main/java/novalogics/android/androidcanvas/app/MainActivity.kt
+++ app/src/main/java/novalogics/android/androidcanvas/app/MainActivity.kt
@@ -3,30 +3,60 @@ package novalogics.android.androidcanvas.app
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import novalogics.android.androidcanvas.ui.ThemeDemoScreen
+import novalogics.android.androidcanvas.ui.colorScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
+//            statusBarStyle = SystemBarStyle.light(
+//                android.graphics.Color.TRANSPARENT,
+//                android.graphics.Color.TRANSPARENT
+//            )
             statusBarStyle = SystemBarStyle.dark(
                 android.graphics.Color.TRANSPARENT
             )
         )
         setContent {
+            SystemUi(windows = window)
             MaterialTheme {
                 ThemeDemoScreen()
             }
         }
     }
 }
+
+@Composable
+fun SystemUi(windows: Window) =
+    MaterialTheme {
+        windows.statusBarColor = colorScheme.background.toArgb()
+        windows.navigationBarColor = Color.Black.toArgb()
+
+        @Suppress("DEPRECATION")
+        if (MaterialTheme.colorScheme.surface.luminance() > 0.5f) {
+            windows.decorView.systemUiVisibility = windows.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        @Suppress("DEPRECATION")
+        if (MaterialTheme.colorScheme.surface.luminance() > 0.5f) {
+            windows.decorView.systemUiVisibility = windows.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+    }
 
 @Preview(
     name = "Light Mode",
